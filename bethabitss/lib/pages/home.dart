5c624a92772f1ec173b2fa332a7125c6d951921e
Mtrjoel <monterojoel135@gmail.com>
2024-09-14 14:02:48 -0400

    restructure of code

--- bethabitss/lib/pages/home.dart
+++ bethabitss/lib/pages/home.dart
@@ -1,7 +1,4 @@
 import 'dart:async';
-import 'package:bethabitss/pages/long_break.dart';
-import 'package:bethabitss/pages/short_break.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart' show GoogleFonts;
 import 'package:responsive_sizer/responsive_sizer.dart';
